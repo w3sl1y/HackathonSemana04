9b5c4f16a94780ed699625514898fc5b441d1091
w3sl1y <SV73623892@idat.edu.pe>
2021-09-18 11:25:59 -0500

    Actualizado

--- index.js
+++ index.js
@@ -15,4 +15,22 @@ function delay(ms) {
   delay(3000).then(() => alert('runs after 3 seconds'));
 
   // Ejercicio #2
+  promise.then(f1).catch(f2);
+  vs
+  promise.then(f1, f2);
+  //No son iguales, porque con el primero, al ocurrir un error, es procesado con catch y de la segunda manera el error no sería manejado.
+
+  // Ejercicio #3
+  async function loadJson(url) {
+    let response = await fetch(url);
   
+    if (response.status == 200) {
+      let json = await response.json();
+      return json;
+    }
+  
+    throw new Error(response.status);
+  }
+  
+  loadJson('no-such-user.json')
+    .catch(alert); // Error: 404
