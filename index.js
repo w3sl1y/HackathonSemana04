9722fe863c0493d1ca802e67385d5b74d23cef3a
w3sl1y <SV73623892@idat.edu.pe>
2021-09-18 12:06:36 -0500

    Actualización final

--- index.js
+++ index.js
@@ -34,3 +34,97 @@ function delay(ms) {
   
   loadJson('no-such-user.json')
     .catch(alert); // Error: 404
+
+// Ejercicio #4
+  class HttpError extends Error {
+    constructor(response) {
+      super(`${response.status} for ${response.url}`);
+      this.name = 'HttpError';
+      this.response = response;
+    }
+  }
+  
+  async function loadJson(url) {
+    let response = await fetch(url);
+    if (response.status == 200) {
+      return response.json();
+    } else {
+      throw new HttpError(response);
+    }
+  }
+
+  async function demoGithubUser() {
+    let user;
+    while(true) {
+      let name = prompt("Ingrese un nombre:", "iliakan");
+  
+      try {
+        user = await loadJson(`https://api.github.com/users/${name}`);
+        break; // sin error, salir del bucle
+      } catch(err) {
+        if (err instanceof HttpError && err.response.status == 404) {
+          // bucle continúa después del alert
+          alert("No existe tal usuario, por favor reingrese.");
+        } else {
+          // error desconocido, lo relanza
+          throw err;
+        }
+      }
+    }
+    alert(`Nombre completo: ${user.name}.`);
+    return user;
+  }
+  
+  demoGithubUser();
+
+  //Ejercicio #5
+  async function wait() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return 10;
+  }
+  
+  function f() {
+    wait().then(result => alert(result));
+  }
+  
+  f();
+
+  //Ejercicio #6
+  new Promise(function(resolve, reject) {
+    setTimeout(() => {
+      throw new Error("Whoops!");
+    }, 1000);
+  }).catch(alert);
+  //No lo disparará, porque el error no es generado cuando el ejecutor está corriendo, sino después.
+
+  // Ejercicio #7
+
+  // 1. Utilizando setInterval
+  function printNumbers(from, to) {
+    let current = from;
+  
+    let timerId = setInterval(function() {
+      alert(current);
+      if (current == to) {
+        clearInterval(timerId);
+      }
+      current++;
+    }, 1000);
+  }
+  
+  printNumbers(1, 10);
+
+  // 2. Usando setTimeout anidado
+  function printNumbers(from, to) {
+    let current = from;
+  
+    setTimeout(function go() {
+      alert(current);
+      if (current < to) {
+        setTimeout(go, 1000);
+      }
+      current++;
+    }, 1000);
+  }
+  
+  printNumbers(1, 10);
